5c6cc272c9be4627761782873201d9caf554648f
reyjrchavez23 <reyjrchavez23@tamu.edu>
2016-10-12 21:15:21 -0500

    testing arduino stuffs

---
+++ test/test.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+/*
+int add(int a, int b){
+  return a +b;
+}
+
+add(1,2);
+
+for(int i = 0; i < array.sizeof(); i++){
+  print.array(i);
+}
+*/
