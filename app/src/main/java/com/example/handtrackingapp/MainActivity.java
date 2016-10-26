d67aa7092ad73356350294d2524741df70d426b6
Kablitz1 <markwager@tamu.edu>
2016-10-26 13:52:22 -0500

    LED Bluetooth App

--- app/src/main/java/com/example/handtrackingapp/MainActivity.java
+++ app/src/main/java/com/example/handtrackingapp/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.handtrackingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log; //for logging console
+
+import android.widget.Button;
+import android.widget.ListView;
+
+import static android.R.attr.id;
+
 // Jim's comment
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Button btnPaired;
+    ListView deviceList;
+
+    btnPaired.findViewById(id.buttonOn);
+
+
+//messing with output to console NOTE::doesnt work yet
+    //Log.v(CONSOLE_TAG, "Testing Console...\n");
 }
